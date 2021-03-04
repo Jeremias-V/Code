2dc5557363790dfebcbaa33c00c4e8e69a39a08a
Jeremias-V <jeremias.villalobos.t@gmail.com>
2021-03-03 20:53:12 -0500

    UVa solution 10706

---
+++ Solutions/UVa/10706.py
@@ -0,0 +1,48 @@
+import math
+from sys import stdin, stdout 
+
+def generateArr(A):
+    i = 1
+    while(len(A) < 150000):
+        if i >= 10:
+            tmp = str(i)
+            for c in tmp:
+                A.append(c)
+        else:
+            A.append(str(i))
+        i += 1
+
+def generateSearchArr(A):
+    A.append(0)
+    digits = 0
+    for i in range(1, 31268):
+        digits += math.floor(math.log10(i))
+        A.append(i + A[i-1] + digits)
+
+def binarySearch(A, i):
+    low, hi = 0, len(A)
+    while low + 1 !=  hi:
+        mid = low + ((hi - low) >> 1)
+        if A[mid] < i:
+            low = mid
+        else:
+            hi = mid
+    ans = low+1
+    return ans
+
+def main():
+    a = int(stdin.readline().strip())
+    S = []
+    ansArr = []
+    generateSearchArr(S)
+    generateArr(ansArr)
+    ans = ""
+    while a > 0:
+        i = int(stdin.readline())
+        k = binarySearch(S, i)
+        before = S[k-1]
+        pos = i - before - 1
+        ans += str(ansArr[pos]) + '\n'
+        a -= 1
+    stdout.write(ans)
+main()
